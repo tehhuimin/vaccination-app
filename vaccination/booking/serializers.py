5c0619dbe36601d293a9f2e591f1736717a1e97a
hm <hm.tehhuimin@gmail.com>
2021-11-12 02:08:04 +0800

    add booking data serializer

--- vaccination/booking/serializers.py
+++ vaccination/booking/serializers.py
@@ -9,3 +9,16 @@ class VaccinationCenterSerializer(serializers.ModelSerializer):
     class Meta: 
         model = VaccinationCenter
         fields = ('id', 'name')
+
+class BookingSerializer(serializers.ModelSerializer):
+    id = serializers.PrimaryKeyRelatedField(read_only=True)
+    name = serializers.CharField(source='NRIC.name')
+    NRIC = serializers.CharField(source='NRIC.NRIC')
+    centerName = serializers.CharField(source='center.name')
+    centerId = serializers.PrimaryKeyRelatedField(source='center.id', read_only=True)
+    startTime = serializers.DateField(source='date')
+    timeSlot = serializers.IntegerField(source='time_slot')
+
+    class Meta: 
+        model = Booking
+        fields = ('id', 'name', 'NRIC', 'centerName', 'centerId', 'startTime', 'timeSlot')
