94521c30b8eb81af02cf194d20082f27ff9381d5
hm <hm.tehhuimin@gmail.com>
2021-11-12 00:54:14 +0800

    init models

--- vaccination/booking/models.py
+++ vaccination/booking/models.py
@@ -1,3 +1,82 @@
 from django.db import models
 
-# Create your models here.
+# Time slots (enum values)
+from enum import IntEnum
+class TimeSlotChoices(IntEnum):
+    AM10_AM11 = 1
+    AM11_PM12 = 2
+    PM12_PM01 = 3
+    PM01_PM02 = 4
+    PM02_PM03 = 5
+    PM03_PM04 = 6
+    PM04_PM05 = 7
+    PM05_PM06 = 8
+
+    @classmethod
+    def choices(cls):
+        return [(key.value, key.name) for key in cls]
+
+# User (Patient)
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    NRIC = models.CharField(max_length=9, primary_key=True)
+
+    def __str__(self):
+        return self.name
+
+# Nurse 
+class Nurse(models.Model):
+    name = models.CharField(max_length=50)
+    NRIC = models.CharField(max_length=9, primary_key=True)
+    speed = models.IntegerField(default=10)
+
+    def __str__(self):
+        return self.name
+
+# Vaccination Center 
+class VaccinationCenter(models.Model): 
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    max_capacity = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+# Nurse Availability
+class NurseAvailability(models.Model): 
+    id = models.AutoField(primary_key=True)
+    NRIC = models.ForeignKey(Nurse, on_delete=models.CASCADE)
+    date = models.DateField()
+    time_slot = models.IntegerField(choices=TimeSlotChoices.choices(), default=TimeSlotChoices.AM10_AM11)
+    center = models.ForeignKey(VaccinationCenter, on_delete=models.CASCADE)
+
+    class Meta: 
+        constraints = [
+            models.UniqueConstraint(fields=['NRIC', 'date', 'time_slot', 'center'], name='nurse_availability')
+        ]
+
+
+# # Vaccination Center Availability
+# class CenterAvailability(models.Model): 
+#     center = models.ForeignKey(VaccinationCenter, on_delete=models.CASCADE)
+#     date = models.DateField()
+#     time_slot = models.IntegerField(choices=TimeSlotChoices.choices(), default=TimeSlotChoices.AM10_AM11)
+#     slot_capaciity = models.IntegerField(default=0)
+#     available_capacity = models.IntegerField(default=0)
+
+#     class Meta: 
+#         constraints = [
+#             models.UniqueConstraint(
+#                 fields=['center', 'date', 'time_slot'], 
+#                 name='vaccination_center_availability'
+#             )
+#         ]
+
+# Booking
+class Booking(models.Model): 
+    center = models.ForeignKey(VaccinationCenter, on_delete=models.CASCADE)
+    date = models.DateField()
+    time_slot = models.IntegerField(choices=TimeSlotChoices.choices(), default=TimeSlotChoices.AM10_AM11)
+    NRIC = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+
+
